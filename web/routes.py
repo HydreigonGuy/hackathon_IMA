1dc56cde0a0ad6dbe7363b66bb3ffcab52da316c
HydreigonGuy <ad.thibault18@gmail.com>
2022-06-25 14:32:09 +0200

    Added home page with image changing with buttons

--- web/routes.py
+++ web/routes.py
@@ -1,7 +1,8 @@
 
 from crypt import methods
+from urllib import response
 from app import app
-from flask import jsonify, render_template
+from flask import jsonify, render_template, request
 
 @app.route('/')
 def home():
@@ -10,5 +11,10 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     ret = {}
-    print('hello')
+    if request.data == b'1':
+        ret["img"] = "https://images.contentstack.io/v3/assets/blt187521ff0727be24/blta27d0cb379d1daf0/60ee1269f7e95137f9d04923/soraka-splash.jpg"
+    if request.data == b'2':
+        ret["img"] = "https://pbs.twimg.com/media/Eiit6JRWAAIsbeB?format=jpg&name=large"
+    if request.data == b'3':
+        ret["img"] = "https://c0.lestechnophiles.com/www.madmoizelle.com/wp-content/uploads/2012/01/trollface.jpg"
     return (jsonify(ret))
--- web/static/js/predict.js
+++ web/static/js/predict.js
@@ -1,11 +1,20 @@
 
-function predict()
+var image_style = 'position: absolute; right: 20px; top: 350px; width: 40%;';
+
+function predict(id)
 {
     fetch("/predict", {
-        method: "POST"
+        method: "POST",
+        body:String(id)
       }).then(res => {
           return(res.json());
       }).then(res => {
+          document.getElementById("board").innerHTML = '<img style="' + image_style + '" src="' + res.img + '">';
           console.log(res);
       });
+}
+
+function reset()
+{
+    document.getElementById("board").innerHTML = "";
 }
