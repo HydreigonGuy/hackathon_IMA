205f9eefb89b1807f196f10764a79c6e0fd88045
HydreigonGuy <ad.thibault18@gmail.com>
2022-06-25 21:54:42 +0200

    Adding date getting in front and sending to backend

--- web/routes.py
+++ web/routes.py
@@ -1,8 +1,11 @@
 
 from crypt import methods
 from urllib import response
+
+from sqlalchemy import JSON
 from app import app
 from flask import jsonify, render_template, request
+import json
 
 @app.route('/')
 def home():
@@ -11,10 +14,10 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     ret = {}
-    if request.data == b'1':
+    if json.loads(request.data.decode())["id"] == 1:
         ret["img"] = "https://images.contentstack.io/v3/assets/blt187521ff0727be24/blta27d0cb379d1daf0/60ee1269f7e95137f9d04923/soraka-splash.jpg"
-    if request.data == b'2':
+    if json.loads(request.data.decode())["id"] == 2:
         ret["img"] = "https://pbs.twimg.com/media/Eiit6JRWAAIsbeB?format=jpg&name=large"
-    if request.data == b'3':
+    if json.loads(request.data.decode())["id"] == 3:
         ret["img"] = "https://c0.lestechnophiles.com/www.madmoizelle.com/wp-content/uploads/2012/01/trollface.jpg"
     return (jsonify(ret))
--- web/static/js/predict.js
+++ web/static/js/predict.js
@@ -3,14 +3,18 @@ var image_style = 'position: absolute; right: 20px; top: 350px; width: 40%;';
 
 function predict(id)
 {
+    body = {
+        'date':document.getElementById("date").value,
+        'id':id
+    };
+    console.log(body);
     fetch("/predict", {
         method: "POST",
-        body:String(id)
+        body:JSON.stringify(body)
       }).then(res => {
           return(res.json());
       }).then(res => {
           document.getElementById("board").innerHTML = '<img style="' + image_style + '" src="' + res.img + '">';
-          console.log(res);
       });
 }
 
@@ -18,3 +22,13 @@ function reset()
 {
     document.getElementById("board").innerHTML = "";
 }
+
+function create_map()
+{
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: 'OpenStreetMap'
+    }).addTo(map);
+}
+
